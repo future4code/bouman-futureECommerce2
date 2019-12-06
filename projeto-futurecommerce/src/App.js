0ec32d6fa658ab08d97c543f607c9f620c04216d
MateusGesualdo <mateusgcs99@gmail.com>
2019-12-06 15:23:19 -0300

    Parte Obrigatória

--- projeto-futurecommerce/src/App.js
+++ projeto-futurecommerce/src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './App.css';
 import Produto from './Components/Produto/Produto'
 import styled from 'styled-components'
-
 const Section = styled.section`
   display:flex;
   flex-wrap:wrap;
 `
-
+//Teste
 const lista = [
   {
     id: 0 ,
@@ -41,7 +40,7 @@ const lista = [
   } ,
   {
     id: 5 ,
-    nome: "Pacote -" ,
+    nome: "Pacote - Giant Squid Nebula" ,
     preco: 46000000 ,
     imagem: "https://miro.medium.com/max/900/1*Mdps-D92jHID_3bFnlyogg.jpeg"
   } ,
@@ -57,56 +56,154 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state ={
-      ordemCrescente: true , 
+      ordem: "Crescente" , 
       quantidade: [0,0,0,0,0,0,0] ,
       filtrosMax: Infinity ,
-      filtrosMin: 0 ,
+      filtrosMin: '' ,
       filtroNome: ''
     }
   }
+somar = () => {
+  let total = 0
+  for (let i=0; i<7; i++) {
+    total += this.state.quantidade[i] * lista[i].preco
+  }
+  return (
+    total
+  )
+}
+removerItem = (event) => {
+  const copy = this.state.quantidade.map((quant, i) => {
+    if (i === Number(event.target.parentNode.id)) {
+      return ( quant-1 )
+    } else {
+      return (quant)
+    }
+  })
+
+  this.setState({quantidade: copy})
+}
+
+adicionarItem = (id) => {
+  const copy = this.state.quantidade.map((quant, i) => {
+    if ( i === id ) {
+      return ( quant+1 )
+    } else {
+      return (quant)
+    }
+  })
+  this.setState({quantidade: copy})
+}
 
   render () {
-
-  
+    
     return (
-      <div className="App">
+      <div className="App"> 
         <section>      
           <fieldset>
-
             <legend>
               <h2>Filtros</h2>
             </legend>
-            <input type="number" placeholder="Valor mínimo:"></input><br/>
-            <input type="number" placeholder="Valor máximo:"></input><br/>
-            <input type="number" placeholder="Buscar produto:"></input><br/>
+            <input 
+              type="number" 
+              placeholder="Valor mínimo:"
+              value={this.state.filtrosMin}
+              onChange={(event)=>{
+                console.log(event.target.value)
+                this.setState({
+                  filtrosMin: event.target.value
+                })
+              }}
+            ></input><br/>
+            <input 
+              type="number" 
+              placeholder="Valor máximo:"
+              value={this.state.filtrosMax}
+              onChange={(event)=>{
+                if (event.target.value) {
+                  this.setState({
+                    filtrosMax: event.target.value
+                  })
+                } else {
+                  this.setState({
+                    filtrosMax: Infinity
+                  })
+                }
+               
+              }}
+            ></input><br/>
+            <input 
+              type="text" 
+              placeholder="Buscar produto:"
+              value={this.state.filtroNome}
+              onChange={(event)=>{
+                console.log(event.target.value)
+                this.setState({
+                  filtroNome: event.target.value
+                })
+              }}
+            ></input><br/>
             {"Exibir em ordem:"}<br/>
-            <select>
+            <select
+              value={this.state.ordem}
+              onChange={(event)=>{
+                console.log(event.target.value)
+                this.setState({
+                  ordem: event.target.value
+                })
+              }}
+            >
               <option>Crescente</option>
               <option>Decrescente</option>
             </select>
-
           </fieldset>
-          <div>
-            <h2>Carrinho:</h2>
-            {"item: "}
-            {" 0 "}
-            {" (remover)"}<br/>
-            {"soma:0"}
-          </div>
+          <details>
+            <summary>
+              <strong>Carrinho:</strong>
+            </summary>
+            {lista.map((produto, i)=>{
+              if (this.state.quantidade[i] > 0)
+              return (
+              <div id={produto.id}>
+                {produto.nome + ":" + " " + this.state.quantidade[i]}
+                <button onClick={this.removerItem}> X </button>
+              </div> )
+            })
+                
+            } 
+            <strong>Total: </strong>
+            {this.somar ()}
+          </details>
         </section>
         <Section>
-          {lista.map((produto)=>
-            <Produto 
-              id={produto.id}
-              imagem={produto.imagem}
-              preco={produto.preco}
-              nome={produto.nome}
-            />
-          )}    
+          {this.state.ordem === "Crescente" ?
+            lista.filter((produto)=>
+              produto.preco > this.state.filtrosMin && produto.preco < this.state.filtrosMax && produto.nome.includes(this.state.filtroNome)
+            ).map((produto)=>
+              <Produto 
+                id={produto.id}
+                imagem={produto.imagem}
+                preco={produto.preco}
+                nome={produto.nome}
+                passarId={this.adicionarItem}                           
+              />
+            )  
+           :  
+           lista.filter((produto)=>
+              produto.preco > this.state.filtrosMin && produto.preco < this.state.filtrosMax && produto.nome.includes(this.state.filtroNome)
+            ).map((produto)=>
+              <Produto 
+                id={produto.id}
+                imagem={produto.imagem}
+                preco={produto.preco}
+                nome={produto.nome}
+                passarId={this.adicionarItem}  
+              />
+            ).reverse()
+          }
         </Section>
       </div>
     );
   }
 }
-
 export default App;
